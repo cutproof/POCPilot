8cb397262fdbe3bf3d3d7efd82dded091ba71869
cutproof <samjsem@yahoo.com>
2019-08-11 22:07:06 -0400

    Updated JMS and File Impl ...

--- src/main/java/com/ilinksolutions/uscis/POCPilotInvoker.java
+++ src/main/java/com/ilinksolutions/uscis/POCPilotInvoker.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ImportResource;
 
+import com.ilinksolutions.uscis.jms.POCJMSRouter;
+
 @SpringBootApplication
 @ImportResource({"classpath:spring/camel-context.xml"})
 public class POCPilotInvoker
@@ -11,5 +13,15 @@ public class POCPilotInvoker
     public static void main(String[] args)
     {
         SpringApplication.run(POCPilotInvoker.class, args);
+        POCJMSRouter jmsRouter = new POCJMSRouter();
+        try
+        {
+			System.out.println("The JMS Route returned: " + jmsRouter.execute());
+		}
+        catch (Exception e)
+        {
+        	System.out.println("The JMS Route returned: " + e.getMessage());
+        	e.printStackTrace();
+		}
     }
 }
---
+++ src/main/java/com/ilinksolutions/uscis/jms/POCJMSRouter.java
@@ -0,0 +1,49 @@
+package com.ilinksolutions.uscis.jms;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+/**
+ * A route that polls an FTP server for new orders, downloads them, converts the order 
+ * file into a JMS Message and then sends it to the JMS incomingOrders queue hosted 
+ * on an embedded ActiveMQ broker instance.
+ */
+public class POCJMSRouter
+{
+	private int returnValue = 0;
+	
+    public int execute() throws Exception
+    {
+        CamelContext context = new DefaultCamelContext();
+        ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://ilink-amq-amq-1-4x5xk:61616");
+        connectionFactory.setUserName("ilink");
+        connectionFactory.setPassword("ilink01");
+        context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+        context.addRoutes(new RouteBuilder() 
+        {
+            @Override
+            public void configure()
+            {
+                from("file:data/outbox?fileName=person.xml&noop=true")
+                .process(new Processor()
+                {                    
+                    public void process(Exchange exchange) throws Exception
+                    {
+                        System.out.println("We just downloaded: " + exchange.getIn().getHeader("CamelFileName"));
+                    }
+                })
+                .to("jms:ilinkq1");
+                returnValue = 1;
+            }
+        });
+        context.start();
+        Thread.sleep(10000);
+        context.stop();
+        return returnValue;
+    }
+}
